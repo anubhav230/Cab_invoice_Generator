288d76a739d4c0ce76cf45161d708a2dd7215b41
anubhav230 <anubhavsingh230@gmail.com>
2020-07-10 15:12:07 +0530

    first commit- welcome to fare calculator

---
+++ src/main/java/com/invoice/genereator/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.invoice.genereator;
+
+public class InvoiceGenerator {
+    public double calculateFare(double distance, int time) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/invoice/genereator/InvoiceServiceTest.java
@@ -0,0 +1,9 @@
+package com.invoice.genereator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+
+
+}
