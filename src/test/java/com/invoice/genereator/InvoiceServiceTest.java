cdbed64660e172a12b64718b9107c246e6b36c1c
anubhav230 <anubhavsingh230@gmail.com>
2020-07-10 16:08:38 +0530

    REFACTORED- InvoiceGenerator method for multiple rides

--- src/test/java/com/invoice/genereator/InvoiceServiceTest.java
+++ src/test/java/com/invoice/genereator/InvoiceServiceTest.java
@@ -3,8 +3,6 @@ package com.invoice.genereator;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.Reader;
-
 public class InvoiceServiceTest {
 
     @Test
@@ -13,7 +11,7 @@ public class InvoiceServiceTest {
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(25, fare,0.0);
+        Assert.assertEquals(25, fare, 0.0);
     }
 
     @Test
@@ -22,7 +20,7 @@ public class InvoiceServiceTest {
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(5, fare,0.0);
+        Assert.assertEquals(5, fare, 0.0);
 
     }
 
@@ -30,11 +28,9 @@ public class InvoiceServiceTest {
     public void givenMultipleRide_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {new Ride(2.0, 5),
-                        new Ride(0.1, 1)
-                    };
+                new Ride(0.1, 1)
+        };
         double fare = invoiceGenerator.calculateFare(rides);
         Assert.assertEquals(30, fare, 0.0);
-
     }
-
 }
