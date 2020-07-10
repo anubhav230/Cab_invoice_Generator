26308605ffee9d48953ec54b205b92eb42f69397
anubhav230 <anubhavsingh230@gmail.com>
2020-07-10 15:20:45 +0530

    ADDED- Test case for calculation distance and time

--- src/test/java/com/invoice/genereator/InvoiceServiceTest.java
+++ src/test/java/com/invoice/genereator/InvoiceServiceTest.java
@@ -5,5 +5,12 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
 
-
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFair() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare,0.0);
+    }
 }
