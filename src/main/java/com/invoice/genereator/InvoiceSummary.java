f22e7f0c79fb7437314dba9593b537790adfa5d6
anubhav230 <anubhavsingh230@gmail.com>
2020-07-10 19:36:08 +0530

    REFACTORED- Test case for invoice summary

---
+++ src/main/java/com/invoice/genereator/InvoiceSummary.java
@@ -0,0 +1,7 @@
+package com.invoice.genereator;
+
+public class InvoiceSummary {
+    public InvoiceSummary(int numOfRides, double totalFair) {
+
+    }
+}
--- src/test/java/com/invoice/genereator/InvoiceServiceTest.java
+++ src/test/java/com/invoice/genereator/InvoiceServiceTest.java
@@ -1,13 +1,20 @@
 package com.invoice.genereator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
 
+    InvoiceGenerator invoiceGenerator;
+
+    @Before
+    public void setUp() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFair() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -16,7 +23,6 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -25,12 +31,12 @@ public class InvoiceServiceTest {
     }
 
     @Test
-    public void givenMultipleRide_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+    public void givenMultipleRide_ShouldReturnInvoiceSummary() {
         Ride[] rides = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(30, fare, 0.0);
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary,summary);
     }
 }
