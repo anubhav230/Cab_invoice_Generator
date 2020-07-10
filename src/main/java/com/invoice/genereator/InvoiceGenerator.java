1470a911aba3d8ef15385e18a780437429bfdcd4
anubhav230 <anubhavsingh230@gmail.com>
2020-07-10 15:26:13 +0530

    ADDED- Code in method for calculating fare

--- src/main/java/com/invoice/genereator/InvoiceGenerator.java
+++ src/main/java/com/invoice/genereator/InvoiceGenerator.java
@@ -1,7 +1,10 @@
 package com.invoice.genereator;
 
 public class InvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KM = 10;
+    private static final int COST_PER_TIME = 1;
+
     public double calculateFare(double distance, int time) {
-        return 0;
+        return distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
     }
 }
